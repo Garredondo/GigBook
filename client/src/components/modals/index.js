c1dc129eda608637c77e832efed007336064b4ca
HunterWilkins <wilkins.hunter@gmail.com>
2019-05-08 11:58:16 -0500

    Styled the Sign Up and Login Modals - OPEN TO DESIGN SUGGESTION

--- client/src/components/modals/index.js
+++ client/src/components/modals/index.js
@@ -1,42 +1,51 @@
-import {React, Component} from 'react';
+import React from 'react';
 import "./style.css";
 function Modal(props) {
     
-    {/* If/Else Statement to show either the Login or Sign Up modal. 
-        Might make more intuitive in future.                     */}
+    /* If/Else Statement to show either the Login or Sign Up modal. 
+        Might make more intuitive in future.                     */
 
-    {/* Subject to change. If we have enough time, we can DRY this up a bit.*/}
+    /* Subject to change. If we have enough time, we can DRY this up a bit.*/
 
-    if (props.type = "login"){
+    if (props.type === "login"){
         return (
-            <div className = "modal">
-                {/* This is just an assumption of the Login modal */}
-                <form>
-                    <h1>Login</h1>
-                    <hr />
-                    <input id = "username-input" type = "text" placeholder = "Username..." />
-                    <input id = "password-input" type = "text" placeholder = "Password..." />
-                    <hr />
-                    <h3>Are you an Artist or a Venue?</h3>
-                    <input type = "radio" />Artist
-                    <input type = "radio" />Venue
-                    <hr />
-                    <button>Log In</button>
-                </form>
-            </div>
+            <div>
+                <div className = "modal">
+                    {/* This is just an assumption of the Login modal */}
+                    <form>
+                        <h1>Login</h1>
+                        <hr />
+                        <input id = "username-input" type = "text" placeholder = "Username..." />
+                        <br />
+                        <input id = "password-input" type = "text" placeholder = "Password..." />
+                        <hr />
+                        <h3>Are you an Artist or a Venue?</h3>
+                        <input type = "radio" />Artist
+                        <input type = "radio" />Venue
+                        <hr />
+                        <button>Log In</button>
+                    </form>
+                </div>
+            <div className = "modal-background" />
+        </div>
         );
     }
 
     else {
         return (
+            <div>
             <div className = "modal">
             {/* This is just an assumption of the Sign Up modal */}
             <form>
                 <h1>Sign Up</h1>
                 <hr />
-                <input id = "username-input" type = "text" placeholder = "Username..." />
-                <input id = "password-input" type = "text" placeholder = "Password..." />
-                <input id = "password-input" type = "text" placeholder = "Confirm Password" />
+                <div id = "input-centering">
+                    <input id = "username-input" type = "text" placeholder = "Username..." />
+                    <br />
+                    <input id = "password-input" type = "text" placeholder = "Password..." />
+                    <br />
+                    <input id = "password-input" type = "text" placeholder = "Confirm Password" />
+                </div>
                 <hr />
                 <h3>Are you an Artist or a Venue?</h3>
                 <input type = "radio" />Artist
@@ -44,7 +53,9 @@ function Modal(props) {
                 <hr />
                 <button>Get Bookin'!</button>
             </form>
-        </div>
+            </div>
+            <div className = "modal-background" />
+            </div>
         );
     }
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Modal from "../components/modals";
 
 
 function Home() {
   return (
     <div>
         <h1>Home Page</h1>
+        <Modal type = "smash"/>
     </div>
   );
 }
