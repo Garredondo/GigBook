3a4a2cf7b5e20bb65327012aea6de7db33c606bd
Wesley Berry <wesleyberry52@gmail.com>
2019-05-08 13:34:19 -0500

    When a user is created, null values and UserId are entered into the Artist table or Venue Table

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,21 +2,51 @@ const router = require("express").Router();
 const db = require("../../models");
 
 router.post("/", function (req, res) {
-
-    db.User.create({
-        name: req.body.name,
-        password: req.body.password,
-        role: req.body.role
-    }).then(function (response) {
-        if (req.body.role == "artist") {
-        res.json(response);
-        } else {
-        res.json(response);
-        }
-        // res.json(response);
-    }).catch(function (err) {
-        res.json(err);
-    });
+    if(req.body.role === "artist") {
+        db.User.create({
+            name: req.body.name,
+            password: req.body.password,
+            role: req.body.role
+        }).then(function(response) {
+                db.Artist.create({
+                    UserId: response.id,
+                    artistName: "",
+                    genre: "",
+                    instrumentation: "",
+                    numberOfMembers: 0,
+                    email: "",
+                    phone: "",
+                    profileImage: "",
+                    website: ""
+            }).then(function (response) {
+                res.json(response);
+            }).catch(function (err) {
+                res.json(err);
+            });
+        });
+    } else {
+        db.User.create({
+            name: req.body.name,
+            password: req.body.password,
+            role: req.body.role
+        }).then(function(response) {
+                db.Venue.create({
+                    UserId: response.id,
+                    venueName: "",
+                    street_address: "",
+                    city: "",
+                    state: "",
+                    zipcode: "",
+                    phone: "",
+                    email: "",
+                    website: "",
+                    image: ""
+            }).then(function (response) {
+                res.json(response);
+            }).catch(function (err) {
+                res.json(err);
+            });
+        });
+    }
 });
-
 module.exports = router;
