873f7f26cb8316fb33540d73277e683f0b4b3a84
ericjosprice <epriceutexas@gmail.com>
2019-05-08 13:29:59 -0500

    adding in gig filter component

---
+++ client/src/components/gigfilter/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./style.css";
+
+
+function gigFilter(props) {
+    return (
+        <form className="input-group mb-3 venue-filter">
+        <div className="input-group-prepend">
+          <button className="btn btn-outline-secondary" type="submit" id="filter">Filter</button>
+        </div>
+        <select name="id" className="custom-select" id="inputGroupSelect03">
+          <option selected>All Venues</option>
+          <option value="{props.venueName}">{props.venueName}</option>
+        </select>
+      </form>
+    );
+}
+
+export default gigFilter;
+
+
+    // $("#filter").on("click",function(event){
+    //     event.preventDefault();
+    //     const name = $("#inputGroupSelect03").val();
+    //     $(".gig-card").each(function(i,element){
+    //         const checkName = $(element).children(".card-text").children(".card-title").text()
+            
+    //         if(name === checkName){
+    //             $(element).show();
+    //         }else if(name === "All Venues"){
+    //             $(element).show();
+    //         }else{
+    //             $(element).hide();
+    //         }
+    //     })
+    // })
+  
--- client/src/pages/ArtistProfile.js
+++ client/src/pages/ArtistProfile.js
@@ -1,22 +1,22 @@
-import React from "react";
-import {LogoutButton, FilterButton, BookGigButton} from "../components/buttons";
+import React, {Component} from "react";
+import {LogoutButton, GigFilter, BookGigButton} from "../components/buttons";
 import { createDecipher } from "crypto";
 
-function ArtistProfile() {
+class ArtistProfile extends Component {
 
 
 
+render(){
+    return (
+      <div>
+          <LogoutButton />
+          <GigFilter />
 
-  return (
-    <div>
-        <LogoutButton />
-        <FilterButton />
-
-      {/* When we map out each gig, 
-      BookGigButton will have dataID={gig.Id} */}
-        <BookGigButton dataId={1}/>
-    </div>
-  );
+        {/* When we map out each gig, 
+        BookGigButton will have dataID={gig.Id} */}
+          <BookGigButton dataId={1}/>
+      </div>
+    );
+  }
 }
-
 export default ArtistProfile;
