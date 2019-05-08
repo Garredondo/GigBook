38fb85ea183e3857351f35576bcdf8e5bde9a136
Wesley Berry <wesleyberry52@gmail.com>
2019-05-08 11:43:09 -0500

    style all buttons

--- client/src/components/buttons/index.js
+++ client/src/components/buttons/index.js
@@ -1,7 +1,65 @@
-import {React, Component} from 'react';
+import React from 'react';
+import "./style.css";
 
-function Button(props) {
+export function ModalButton({
+    className,
+    dataEventTarget,
+    label
+    }) {
     return (
-        <button className = {props.className}>{props.label}</button>
+        <button className={`${className} btn btn-primary btn-lg`} dataToggle="modal"
+        dataTarget={dataEventTarget}>{label}</button>
+    );
+}
+
+export function FormButton({
+    id,
+    value,
+    className,
+    label
+    }) {
+    return (
+        <button id={id} type="submit" value={value}
+        className={`${className} btn btn-primary btn-lg`}>{label}</button>
+    );
+}
+
+export function LogoutButton() {
+    return (
+        <button className="btn btn-primary btn-lg logout">
+        Logout</button>
+    );
+}
+
+export function PostGigButton() {
+    return (
+        <button className="btn btn-primary btn-lg btn-main" 
+        id="gig-create" type="submit" value="Post-Gig">Post Gig</button>
+    );
+}
+
+export function FilterButton() {
+    return (
+        <button className="btn btn-outline-secondary"
+        type="submit" id="filter">Filter</button>
+    );
+}
+
+export function BookGigButton({
+    dataId
+    }) {
+    return (
+        <button className="btn btn-primary btn-lg btn-main book card-button" 
+        dataId={dataId}>Book</button>
+    );
+}
+
+export function StartButton({
+    id,
+    label
+    }) {
+    return (
+        <button className="btn btn-primary btn-lg btn-scnd"
+        id={id}>{label}</button>
     );
 }
