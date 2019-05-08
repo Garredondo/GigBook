fb95d4fbecc95dbc15f3ad9e54fce89585cb9b9c
ericjosprice <epriceutexas@gmail.com>
2019-05-08 13:49:32 -0500

    working on gigs filter

--- client/src/pages/ArtistProfile.js
+++ client/src/pages/ArtistProfile.js
@@ -1,22 +1,47 @@
 import React, {Component} from "react";
-import {LogoutButton, GigFilter, BookGigButton} from "../components/buttons";
+import {LogoutButton, FilterButton, BookGigButton} from "../components/buttons";
 import { createDecipher } from "crypto";
 
 class ArtistProfile extends Component {
 
-
-
-render(){
-    return (
-      <div>
-          <LogoutButton />
-          <GigFilter />
-
-        {/* When we map out each gig, 
-        BookGigButton will have dataID={gig.Id} */}
-          <BookGigButton dataId={1}/>
-      </div>
-    );
+  FilterButtion() {
+    gigsFilter(); 
   }
+
+  gigsFilter(){
+      return (
+          <form className="input-group mb-3 venue-filter">
+          <div className="input-group-prepend">
+            <button className="btn btn-outline-secondary" type="submit" id="filter">Filter</button>
+          </div>
+          <select name="id" className="custom-select" id="inputGroupSelect03">
+            <option selected>All Venues</option>
+            <option value="{props.venueName}">{props.venueName}</option>
+          </select>
+        </form>
+      );
+
+  }
+
+
+
+
+
+
+
+
+
+  render(){
+      return (
+        <div>
+            <LogoutButton />
+            <FilterButton />
+
+          {/* When we map out each gig, 
+          BookGigButton will have dataID={gig.Id} */}
+            <BookGigButton dataId={1}/>
+        </div>
+      );
+    }
 }
 export default ArtistProfile;
