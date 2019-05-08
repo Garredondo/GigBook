e099ef274efbd22d0d649248c73b8894159c9054
HunterWilkins <wilkins.hunter@gmail.com>
2019-05-08 14:38:45 -0500

    Made changes

--- client/src/components/cards/index.js
+++ client/src/components/cards/index.js
@@ -1,18 +1,30 @@
-import {React, Component} from 'react';
+import React from 'react';
+import {BookGigButton} from "../buttons";
+import "./style.css";
 
 function ResultBox(props) {
     return (
         <div className = "result-box">
-            <img src = {props.src} alt = "result image"/>
-
+            <div className = "image-wrapper">
+                <img src = {props.src} alt = "result image"/>
+            </div>
+            
             {/* either the artist's name/description/genre or the venue's */}
+            
+            
             <h3>{props.name}</h3>
-            <p>{props.description}</p>
-            <p>{props.genre}</p>
+            <hr />
+            <div className = "info">
+            <p className = "genre"><em>{props.genre}</em></p>
+            <p class = "date">{props.date}</p>
+            <br />
+            <p className = "description">{props.description}</p>
+            
             {/*===========================================================*/}
 
-            <p>{props.date}</p>
-            <button>Book it!</button>
+            
+            </div>
+            <BookGigButton />
         </div>
     );
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,34 +1,20 @@
 import React, {Component} from "react";
-import {ModalButton, FormButton} from "../components/buttons";
-
+import ResultBox from "../components/cards/index.js";
 
 class Home extends Component {
-
-
-
 
   render() { 
     return (
       <div>
-          <ModalButton className={"log-in"} 
-          data-target={"#login-modal"}
-          label={"Log In"}/>
-          <ModalButton className={"sign-up"} 
-          data-target={"#form-modal"}
-          label={"Sign Up"}/>
-          <ModalButton className={"sign-up-main"} 
-          data-target={"#form-modal"}
-          label={"Sign Up"}/>
-          <FormButton id={"login-submit"} value={"Submit"} 
-          className={"log-in"}
-          label={"Log In"}/>
-          <FormButton id={"signup-submit"} type={"submit"} value={"Submit"}
-          className={"sign-up-main"} 
-          label={"Sign Up"}/>
+        <ResultBox 
+        src = "https://cdn.shopify.com/s/files/1/1365/2497/products/12676-BananaSlugMask-Sky_900x.jpg?v=1520535633"
+        name = "Logan's Snack Shack" 
+        description = "A funky little venue where people come together to honor a man named 'Loggy', who, unfortunately, to his and everyone's dismay, tripped over a log and died." 
+        genre = "alternative" 
+        date = "Nov 5th"/>
       </div>
-      
     );
-  };
+  }
 }
 
 export default Home;
