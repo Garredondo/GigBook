72cdaf5d2c7cc5d89ec926ac428d9241c884cb34
ericjosprice <epriceutexas@gmail.com>
2019-05-14 10:54:16 -0500

    adjusted height of requested gig card to 375px

--- client/src/components/requestedCards/index.js
+++ client/src/components/requestedCards/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import moment from "moment";
+import "./style.css";
 
 
 function ResultBox2(props) {
     return (
-        <div className = "gig-card col-12">
+        <div className = "gig-card requests col-12">
         <img src = {props.src} alt = "Venue's profile image" className= "image"/>
 
         <div className = "card-text">
